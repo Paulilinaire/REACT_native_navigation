1608e148d8f85e433d5dc4b102f535f26d86a9c9
Pauline Laout <laout.pauline@hotmail.fr>
2023-11-09 16:47:13 +0100

    demoGPS

--- demoGPS/App.js
+++ demoGPS/App.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, SafeAreaView, FlatList } from 'react-native'
+import {StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Geolocation from '@react-native-community/geolocation'
-import axios from 'axios'
+
+
+// npm i @react-native-community/geolocation
+// ajout de la ligne : <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+// dans le repertoire android/app/src/main/AndroidManifest.xml
+// npm i axios
 
 export default function App() {
-    const [city, setCity] = useState()
+
     const [latitude, setLatitude] = useState(null)
     const [longitude, setLongitude] = useState(null)
 
-    
     useEffect(() => {
         Geolocation.requestAuthorization()
         Geolocation.getCurrentPosition(position => {
@@ -16,23 +20,18 @@ export default function App() {
             setLongitude(position.coords.longitude)
         },
         error => console.log(error),
-        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
-    },[])
-    
-    useEffect(() => {
-        axios.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=vDOKZJIZJer5jd3EUl7UfWMTKwPQm0vd&q=15.8%2C8.00&language=en-us&details=false&toplevel=false')
-        .then(response => {
-            const data = response.data.LocalizedName
-            setCity(data)
-        })
+        {enableHighAccuracy : true, timeout : 20000,maximumAge : 1000 }
+        )
     },[])
 
+
   return (
-    <SafeAreaView>
+    <View>
+
       <Text>Latitude : {latitude}</Text>
       <Text>Longitude : {longitude}</Text>
-      <Text>Ville: {city}</Text>
-    </SafeAreaView>
+
+    </View>
   )
 }
 
