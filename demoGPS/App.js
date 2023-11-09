57c6408d487538f7c6c8db871a1b526233f372e8
Pauline Laout <laout.pauline@hotmail.fr>
2023-11-09 15:59:14 +0100

    update

--- demoGPS/App.js
+++ demoGPS/App.js
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Geolocation from '@react-native-community/geolocation'
+import axios from 'axios'
 
 export default function App() {
-
+    const [city, setCity] = useState()
     const [latitude, setLatitude] = useState(null)
     const [longitude, setLongitude] = useState(null)
 
+    
     useEffect(() => {
         Geolocation.requestAuthorization()
         Geolocation.getCurrentPosition(position => {
@@ -16,11 +18,20 @@ export default function App() {
         error => console.log(error),
         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
     },[])
+    
+    useEffect(() => {
+        axios.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=vDOKZJIZJer5jd3EUl7UfWMTKwPQm0vd&q=15.8%2C8.00&language=en-us&details=false&toplevel=false')
+        .then(response => {
+            const data = response.data.LocolizedName
+            setCity(data)
+        })
+    },[])
 
   return (
     <SafeAreaView>
       <Text>Latitude : {latitude}</Text>
       <Text>Longitude : {longitude}</Text>
+      <Text>Ville: {data}</Text>
     </SafeAreaView>
   )
 }
